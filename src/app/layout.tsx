4968559e60714c3b19a31075a7244ae59d1cf344
janddrras <janddrras@gmail.com>
2024-05-04 13:56:12 +0300

    home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
-      <body className={cn("relative h-full font-sans antialiased", inter.className)}>
+      <body className={cn("relative h-full antialiased", inter.className)}>
         <main className="relative flex flex-col min-h-screen">
           {/* <Providers> */}
           {/* <Navbar /> */}
