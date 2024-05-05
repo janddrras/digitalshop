1e5d52aeeb015e0cef79e9e005af2b4f1022a351
janddrras <janddrras@gmail.com>
2024-05-05 17:37:43 +0300

    payload setup

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express from "express"
+import { getPayloadClient } from "./payload/getPayloadClient"
+import { nextApp, nextHandler } from "./nextApp"
+
+const app = express()
+const PORT = Number(process.env.PORT) || 3000
+
+const startServer = async () => {
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
+      }
+    }
+  })
+
+  app.use((req, res) => nextHandler(req, res))
+
+  nextApp.prepare().then(() => {
+    payload.logger.info("Next.js started")
+
+    app.listen(PORT, async () => {
+      payload.logger.info(`Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`)
+    })
+  })
+}
+
+startServer()
