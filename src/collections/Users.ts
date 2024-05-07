321fba63d60bcb97aeec4ea5c6c1e47b0419c22d
janddrras <janddrras@gmail.com>
2024-05-07 14:13:12 +0300

    email verification

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -13,18 +13,13 @@ const adminsAndUser: Access = ({ req: { user } }) => {
 
 export const Users: CollectionConfig = {
   slug: "users",
-  auth: true,
-  // auth: {
-  //   verify: {
-  //     generateEmailHTML: ({ token }) => {
-  //       return PrimaryActionEmailHtml({
-  //         actionLabel: "verify your account",
-  //         buttonText: "Verify Account",
-  //         href: `${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`
-  //       })
-  //     }
-  //   }
-  // },
+  auth: {
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        return `<a href="${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}">Click this link to verify your email address</a>`
+      }
+    }
+  },
   access: {
     read: adminsAndUser,
     create: () => true,
