4ac004b9d33123654605327bf91a2534dbe616a8
janddrras <janddrras@gmail.com>
2024-05-10 12:29:53 +0300

    Cart complete

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -2,25 +2,36 @@
 
 import { ShoppingCart } from "lucide-react"
 import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet"
-import Link from "next/link"
-import { buttonVariants } from "@/components/ui/button"
-import Image from "next/image"
-import { ScrollArea } from "@/components/ui/scroll-area"
 import { Separator } from "@/components/ui/separator"
 import { formatPrice } from "@/lib/utils"
+import Link from "next/link"
+import { Button, buttonVariants } from "@/components/ui/button"
+import Image from "next/image"
+import { useCart } from "@/hooks/useCart"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import CartItem from "./CartItem"
+import { useEffect, useState } from "react"
 
 const Cart = () => {
-  const isMounted = false
-  const itemCount = 1
-  const fee = 0
-  const cartTotal = 0
+  const { items, clearCart } = useCart()
+  const itemCount = items.length
+
+  const [isMounted, setIsMounted] = useState<boolean>(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  const cartTotal = items.reduce((total, { product }) => total + product.price, 0)
+
+  const fee = 1
+
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
         <ShoppingCart aria-hidden="true" className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
         <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">{isMounted ? itemCount : 0}</span>
       </SheetTrigger>
-
       <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
         <SheetHeader className="space-y-2.5 pr-6">
           <SheetTitle>Cart ({itemCount})</SheetTitle>
@@ -29,9 +40,9 @@ const Cart = () => {
           <>
             <div className="flex w-full flex-col pr-6">
               <ScrollArea>
-                {/* {items.map(({ product }) => (
+                {items.map(({ product }) => (
                   <CartItem product={product} key={product.id} />
-                ))} */}
+                ))}
               </ScrollArea>
             </div>
             <div className="space-y-4 pr-6">
@@ -52,11 +63,21 @@ const Cart = () => {
               </div>
 
               <SheetFooter>
-                <SheetTrigger asChild>
-                  <Link href="/cart" className={buttonVariants({ className: "w-full" })}>
-                    Continue to Checkout
-                  </Link>
-                </SheetTrigger>
+                <div className="flex flex-col w-full space-y-4">
+                  <SheetTrigger asChild>
+                    <Link
+                      href="/cart"
+                      className={buttonVariants({
+                        className: "w-full"
+                      })}
+                    >
+                      Continue to Checkout
+                    </Link>
+                  </SheetTrigger>
+                  <Button variant="link" size="sm" className="text-sm text-destructive" onClick={() => clearCart()}>
+                    Empty the Cart
+                  </Button>
+                </div>
               </SheetFooter>
             </div>
           </>
@@ -69,7 +90,11 @@ const Cart = () => {
             <SheetTrigger asChild>
               <Link
                 href="/products"
-                className={buttonVariants({ variant: "link", size: "sm", className: "text-sm text-muted-foreground" })}
+                className={buttonVariants({
+                  variant: "link",
+                  size: "sm",
+                  className: "text-sm text-muted-foreground"
+                })}
               >
                 Add items to your cart to checkout
               </Link>
